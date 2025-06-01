dbb43bd8ff86872133c425deae62ec7986717d00
Isoo Pal <17isoo12@gmail.com>
2025-06-01 12:03:54 +0530

    pyhton Turtle library

---
+++ turtleLibrarySpringBoard/first.py
@@ -0,0 +1,11 @@
+# Python program to draw square 
+# using Turtle Programming
+import turtle 
+import time
+skk = turtle.Turtle()
+skk.speed(1)
+for i in range(4):
+    skk.forward(50)
+    skk.right(90)
+    
+turtle.done()
---
+++ turtleLibrarySpringBoard/star.py
@@ -0,0 +1,12 @@
+import turtle
+import time 
+
+star = turtle.Turtle()
+turtle.shape('turtle')
+star.right(75)
+star.forward(100)
+for i in range(4):
+    star.right(144)
+    star.forward(100)
+
+turtle.done()
