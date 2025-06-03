6d06bc14dba08103536f108a5d59ce032851778d
Isoo Pal <17isoo12@gmail.com>
2025-06-03 16:04:36 +0530

    Tuple and Tuple methods

---
+++ tuple-methods.py
@@ -0,0 +1,16 @@
+t = (1, 2, 3, 2, 2)
+
+print(t.count(3))   #counts no of occurances of given element
+print(t.index(2))   #gives index of given value
+
+a,b,c = (1,3,4)
+print(a)
+print(b)
+print(c)
+
+print(3 in t)
+print(t[1:3])
+
+repeated = t*2
+print(repeated)
+print(len(repeated))
---
+++ tupleExamples.py
@@ -0,0 +1,11 @@
+t1 = ("Ram", 4,5,6.8, "Raman")
+
+print(t1)
+print(type(t1))
+
+t2 = ()   #empty tuple
+
+t3 = (1,)   #Tuple with only one element
+
+print(type(t2))
+print(type(t3))
