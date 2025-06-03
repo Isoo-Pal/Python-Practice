91b1bbeef177b081a4968432a025d4a8877e785f
Isoo Pal <17isoo12@gmail.com>
2025-06-03 15:47:23 +0530

    Some method of list

---
+++ list-methods.py
@@ -0,0 +1,14 @@
+l1 = ["Apple", "Orange", 4, 89.23, False, "Papaya"]
+l1.append("Monica")
+print(l1)
+
+l2 = [1,2,3,4,5,6.7,5.9,0,1.0, 0.345, 0.56]
+l2.sort()
+print(l2)
+l2.reverse()
+print(l2)
+l2.insert(3, 8.9)
+print(l2)
+print(l2.pop(2))
+l2.remove(6.7)
+print(l2)
