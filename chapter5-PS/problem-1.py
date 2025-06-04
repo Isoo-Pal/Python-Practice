4756e4aedf027e3307659abcc801f19019094ef4
Isoo Pal <17isoo12@gmail.com>
2025-06-04 16:14:23 +0530

    Chapter-5 Practice Set

---
+++ chapter5-PS/problem-1.py
@@ -0,0 +1,9 @@
+words = {
+    "kursi" : "Chair",
+    "Kutta" : "Dog",
+    "Mej" : "Table",
+    "Tabka" : "Mango"
+}
+word = input("Enter the word you want engish meaning of : ")
+
+print(words[word])
---
+++ chapter5-PS/problem-3.py
@@ -0,0 +1,4 @@
+# can a set has 18(int) and '18'(string)
+
+s = {1,23.5,6,18, '18', 18.0}
+print(s)
